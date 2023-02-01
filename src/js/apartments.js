81a3a66ba221526081b4a9c53199733162ed8642
Igor Anić <igor.anic@gmail.com>
2023-02-01 14:55:38 +0100

    add apartments table

---
+++ src/js/apartments.js
@@ -0,0 +1,221 @@
+const data = [
+  {
+    stan: "A 1-1",
+    kat: 0,
+    ulaz: "A",
+    kuhinja: 8,
+    boravak: 26.7,
+    soba: [7.8, 12.7],
+    kupaonica: [4.30],
+    terasa: [37.8],
+    vrt: [32.60],
+    prodajno: 75.51,
+    ukupno: 100.6,
+    cijena: 3500
+  },
+
+  {
+    stan: "A 1-2",
+    kat: 0,
+    ulaz: "A",
+    kuhinja: 6.5,
+    boravak: 31,
+    soba: [9.2, 12.3],
+    kupaonica: [4.4],
+    terasa: [41.60],
+    vrt: [22.6],
+    prodajno: 81.23,
+    ukupno: 132.95,
+    cijena: 3500
+  },
+
+  {
+    stan: "B 1-1",
+    kat: 0,
+    ulaz: "B",
+    kuhinja: 7.4,
+    boravak: 23.4,
+    soba: [9.2, 11.4],
+    kupaonica: [4.4],
+    loggia: [7.1],
+    terasa: [17.1],
+    vrt: [9.4],
+    prodajno: 71.94,
+    ukupno: 95,
+    cijena: 3500
+  },
+
+  {
+    stan: "B 1-2",
+    kat: 0,
+    ulaz: "B",
+    kuhinja: 6.5,
+    boravak: 31.4,
+    soba: [9.2, 11.4],
+    kupaonica: [4.5],
+    terasa: [41.6],
+    vrt: [11],
+    prodajno: 79.25,
+    ukupno: 120.35,
+    cijena: 3500
+  },
+
+  {
+    stan: "C 1-1",
+    kat: 0,
+    ulaz: "C",
+    kuhinja: 8,
+    boravak: 26.7,
+    soba: [7.8, 12.7],
+    kupaonica: [4.5],
+    terasa: [26.2],
+    vrt: [10.8],
+    prodajno: 70.63,
+    ukupno: 100,
+    cijena: 3500
+  },
+
+  {
+    stan: "C 1-2",
+    kat: 0,
+    ulaz: "C",
+    kuhinja: 6.8,
+    boravak: 24.4,
+    soba: [9.2, 12.3],
+    kupaonica: [4.4],
+    terasa: [20.9],
+    vrt: [5.5],
+    prodajno: 73.50,
+    ukupno: 95.90,
+    cijena: 3500
+  },
+
+  {
+    stan: "A 2-1",
+    kat: 1,
+    ulaz: "A",
+    kuhinja: 7.4,
+    boravak: 20,
+    soba: [8.1, 12.7],
+    kupaonica: 4.5,
+    loggia: 6.5,
+    prodajno: 61.18,
+    ukupno: 62.80,
+    cijena: 3500
+  },
+
+  {
+    stan: "A 2-2",
+    kat: 1,
+    ulaz: "A",
+    kuhinja: 6.5 ,
+    boravak: 24.8,
+    soba: [9.2, 12.3],
+    kupaonica: 4.4,
+    loggia: 7.1,
+    prodajno: 67.68,
+    ukupno: 69.45,
+    cijena: 3500
+  },
+
+  {
+    stan: "B 2-1",
+    kat: 1,
+    ulaz: "B",
+    kuhinja: 6.5,
+    boravak: 24.8,
+    soba: [9.2, 11.4],
+    kupaonica: 4.4,
+    loggia: 7.1,
+    prodajno: 66.78,
+    ukupno: 68.55,
+    cijena: 3500
+  },
+
+  {
+    stan: "B 2-2",
+    kat: 1,
+    ulaz: "B",
+    kuhinja: 6.5,
+    boravak: 24.8,
+    soba: [9.2, 11.4],
+    kupaonica: 4.5,
+    loggia: 7.1,
+    prodajno: 66.88,
+    ukupno: 68.65,
+    cijena: 3500
+  },
+
+  {
+    stan: "C 2-1",
+    kat: 1,
+    ulaz: "C",
+    kuhinja: 7.4,
+    boravak: 20,
+    soba: [8.1, 12.7],
+    kupaonica: 4.5,
+    terasa: 6.5,
+    prodajno: 57.93,
+    ukupno: 62.8,
+    cijena: 3500
+  },
+
+  {
+    stan: "C 2-2",
+    kat: 1,
+    ulaz: "C",
+    kuhinja: 6.5,
+    boravak: 24.8,
+    soba: [9.2, 12.3],
+    kupaonica: 4.4,
+    loggia: 7.1,
+    prodajno: 67.68,
+    ukupno: 69.45,
+    cijena: 3500
+  },
+
+  {
+    stan: "A 3-1",
+    kat: 2,
+    ulaz: "A",
+    kuhinja: 8.8 ,
+    boravak: 38.7,
+    soba: [11.8, 11.2, 9.6],
+    kupaonica: [5.2, 5.1],
+    terasa: [22.4, 21.3],
+    prodajno: 108.63,
+    ukupno: 139.9,
+    cijena: 3800
+  },
+
+  {
+    stan: "B 3-1",
+    kat: 2,
+    ulaz: "B",
+    kuhinja: 8.6,
+    boravak: 24.6,
+    soba: [10.5, 11.8, 11.2, 10.6],
+    kupaonica: [5.2, 5.1, 2.1],
+    terasa: [19.3, 19.3],
+    prodajno: 115.75,
+    ukupno: 144.7,
+    cijena: 3800
+  },
+
+  {
+    stan: "C 3-1",
+    kat: 2,
+    ulaz: "C",
+    kuhinja: 8.3,
+    boravak: 23.8,
+    soba: [17.8, 14.2, 10.5],
+    kupaonica: [8.4, 2.1],
+    terasa: [21.3, 20.4],
+    prodajno: 109.03,
+    ukupno: 140.3,
+    cijena: 3800
+  }
+
+];
+
+export default data;
--- src/js/main.js
+++ src/js/main.js
@@ -16,94 +16,7 @@ const template = Handlebars.compile("Name: {{name}}");
 console.log(template({ name: "Nils" }));
 
 
-
-const data = [
-  {
-    stan: "A 1-1",
-    kat: 0,
-    ulaz: "A",
-    kuhinja: 8,
-    boravak: 26.7,
-    soba: [7.8, 12.7],
-    kupaonica: [4.30],
-    terasa: [37.8],
-    vrt: [32.60],
-    prodajno: 75.51,
-    ukupno: 100.6
-  },
-
-  {
-    stan: "A 1-2",
-    kat: 0,
-    ulaz: "A",
-    kuhinja: 6.5,
-    boravak: 31,
-    soba: [9.2, 12.3],
-    kupaonica: [4.4],
-    terasa: [41.60],
-    vrt: [22.6],
-    prodajno: 81.23,
-    ukupno: 132.95
-  },
-
-  {
-    stan: "B 1-1",
-    kat: 0,
-    ulaz: "B",
-    kuhinja: 7.4,
-    boravak: 23.4,
-    soba: [9.2, 11.4],
-    kupaonica: [4.4],
-    loggia: [7.1],
-    terasa: [17.1],
-    vrt: [9.4],
-    prodajno: 71.94,
-    ukupno: 95
-  },
-
-  {
-    stan: "B 1-2",
-    kat: 0,
-    ulaz: "B",
-    kuhinja: 6.5,
-    boravak: 31.4,
-    soba: [9.2, 11.4],
-    kupaonica: [4.5],
-    terasa: [41.6],
-    vrt: [11],
-    prodajno: 79.25,
-    ukupno: 120.35
-  },
-
-  {
-    stan: "C 1-1",
-    kat: 0,
-    ulaz: "C",
-    kuhinja: 8,
-    boravak: 26.7,
-    soba: [7.8, 12.7],
-    kupaonica: [4.5],
-    terasa: [26.2],
-    vrt: [10.8],
-    prodajno: 70.63,
-    ukupno: 100
-  },
-
-  {
-    stan: "C 1-2",
-    kat: 0,
-    ulaz: "C",
-    kuhinja: 6.8,
-    boravak: 24.4,
-    soba: [9.2, 12.3],
-    kupaonica: [4.4],
-    terasa: [20.9],
-    vrt: [5.5],
-    prodajno: 73.50,
-    ukupno: 95.90
-  },
-
-];
+import data from "./apartments.js"
 
 
 
@@ -111,27 +24,31 @@ const rowTemplate = Handlebars.compile(`
 <tr>
   <td class="text-nowrap">{{stan}}</td>
   <td class="text-center">{{kat}}</td>
-  <td class="text-center">{{ulaz}}</td>
+
   <td class="text-center">{{soba}}</td>
-  <td class="text-end">{{prodajno}}</td>
+  <td class="text-center">{{prodajno}}</td>
+  <td class="text-center">{{cijena}}</td>
+  <td class="text-center">{{iznos}}</td>
+  <td class="text-center">&nbsp</td>
 
-  <td class="text-end">{{boravak}}</td>
-  <td class="text-end">{{kuhinja}}</td>
-  <td class="text-end">{{sobe}}</td>
-  <td class="text-end">{{kupaonica}}</td>
+  <td class="text-center">{{boravak}}</td>
+  <td class="text-center">{{kuhinja}}</td>
+  <td class="text-center">{{sobe}}</td>
+  <td class="text-center">{{kupaonica}}</td>
 
-  <td class="text-end">{{loggia}}</td>
-  <td class="text-end">{{terasa}}</td>
-  <td class="text-end">{{vrt}}</td>
+  <td class="text-center">{{loggia}}</td>
+  <td class="text-center">{{terasa}}</td>
+  <td class="text-center">{{vrt}}</td>
 
-  <td class="text-end">{{ukupno}}</td>
+  <td class="text-center">{{ukupno}}</td>
+  <td class="text-center">{{prodajno}}</td>
 </tr>
 `);
 
 const content = data.map(d =>
   rowTemplate({
     stan: d.stan,
-    kat: d.kat,
+    kat: d.kat == 0 ? "PR" : d.kat,
     ulaz: d.ulaz,
     soba: d.soba.length,
     boravak: fCurrency(d.boravak),
@@ -143,6 +60,8 @@ const content = data.map(d =>
     terasa: fAreas(d.terasa),
     vrt: fAreas(d.vrt),
     ukupno: fCurrency(d.ukupno),
+    cijena: fCurrency(d.cijena),
+    iznos: fCurrency(d.cijena * d.prodajno)
   })
 ).join(" ");
 
@@ -155,6 +74,9 @@ function fCurrency(num) {
 function fAreas(a) {
   if (!a) {
     return "-";
+  }
+  if (!Array.isArray(a)) {
+    return fCurrency(a);
   }
   if (a.length == 1) {
     return fCurrency(a[0]);
